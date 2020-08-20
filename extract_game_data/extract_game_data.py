aad17c62fd0b18b2a0fb7439700642732823a5c6
Dan <danjbailey14@gmail.com>
2020-08-19 21:35:04 -0700

    Changed format of flat_schema.json and decided to combine schema map into the same file because I didn't want to copy and paste values. General way to interpret all of this is to... think of colons (":") as glue for key/value pairs (e.g. "Dan:Bailey" = {"dan":"bailey"}); think of arrows ("=>") indicating hierarchy (e.g. "Dan:Bailey=>Bruce"); the string after an arrow ("=>") and *without* a colon (":") and accompanying value indicates the final key for the value we need to map (e.g. "Dan:Bailey=>Bruce", we want to know what the value is for {"bruce": ?}); a colon (":") before a word (e.g. ":dogs") indicates an orphaned value in the hierarchy (e.g. "Dan:Bailey=>:dogs") as a means to identify the next node in the path -- similarly a colon (":") after a word (e.g. "GroupName:") indicates a key without a value/a singleton in the hierarchy (e.g. "Dan:Bailey=>GroupName:") as a means to identify the next node in the path.

--- extract_game_data/extract_game_data.py
+++ extract_game_data/extract_game_data.py
@@ -1,6 +1,7 @@
 
 # TODO: Transform EXML to readable and transferable data
-# Input: game file list config
+# Input: game file list config [done]
+# Input: EXML files [done]
 # Input: Conversion spec/map
-# Input: Flattened key/pair schema config
+# Input: Flattened key/pair schema config [done]
 # Output: transformed data saved to .json
